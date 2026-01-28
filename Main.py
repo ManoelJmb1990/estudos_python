907c231fbce40ffba1442487ce29d2e3cbe543a2
Manoel Jorge <manoel.jmb1990@gmail.com>
2026-01-28 18:27:55 -0300

    Exercício da primeira aula, utilização do print()

--- Main.py
+++
@@ -1,7 +0,0 @@
-saldo = 10
-conta = 50
-limite = 150
-
-print(saldo <= limite and conta >=0)
-print(saldo <= limite or conta <0)
-print(not(saldo < limite))
---
+++ day_01/day_01_exercise.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+# This is a simple Python script that prints "Hello World!" to the console.
+
+#Prática de impressão
+
+#Escreva um programa que utilize instruções de impressão para imprimir a seguinte 
+# receita no console de saída. O texto a ser impresso já está presente; você só precisa 
+# transformá-lo em código. Seu código deve imprimir todas as cinco linhas exatamente 
+# como no exemplo de saída abaixo. Certifique-se de não alterar nenhum dos textos 
+# existentes, pois tudo, incluindo pontuação e maiúsculas/minúsculas, precisa ser idêntico!
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
