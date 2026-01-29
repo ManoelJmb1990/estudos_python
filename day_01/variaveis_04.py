1fa4ea6cf29c05d331dd77274235654633380505
Manoel Jorge <manoel.jmb1990@gmail.com>
2026-01-28 23:02:33 -0300

    Foi feito exercícios de input, com atribuição em variáveis e testada a função len() - variaveis_04

---
+++ day_01/variaveis_04.py
@@ -0,0 +1,12 @@
+name = input("What is your name? ")
+print(len(name))  # A função len() imprimirá o comprimento da string armazenada na variável name.
+
+print(len(input("What is your name? ")))
+# No código acima, a função len() é aplicada diretamente ao valor retornado pela função input
+
+username = input("Enter your username: ")
+length = len(username)
+
+
+print(username)
+print(length)
