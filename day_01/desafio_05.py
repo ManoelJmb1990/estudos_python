e3039ff2491316b609cbf9962bec1ffec8525872
Manoel Jorge <manoel.jmb1990@gmail.com>
2026-01-29 10:40:56 -0300

    Foi realizado o desafio do dia 01, de variáveis.

---
+++ day_01/desafio_05.py
@@ -0,0 +1,16 @@
+glass1 = "milk"
+glass2 = "juice"
+
+glass3 = glass1
+glass1 = glass2
+glass2 = glass3
+
+print("glass1:", glass1)
+print("glass2:", glass2)    
+
+
+# Temos duas variáveis, `glass1` e `glass2`. `glass1` 
+# contém leite e `glass2` contém suco. Escreva 3 linhas de 
+# código para trocar o conteúdo das variáveis. Não é permitido 
+# digitar as palavras "leite" ou "suco" . Você só pode usar variáveis ​
+# ​para resolver este exercício.
