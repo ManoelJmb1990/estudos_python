e273c5a985072e4462eebd61e7c5f1bda23c405d
Manoel Jorge <manoel.jmb1990@gmail.com>
2026-01-29 13:46:44 -0300

    Type Error

---
+++ day_02/type_erro_checking.py
@@ -0,0 +1,6 @@
+len(123456) 
+# TypeError: object of type 'int' has no len()
+# acima é um exemplo de TypeError, que ocorre quando uma operação ou função é 
+# aplicada a um objeto de um tipo inadequado.
+
+size = len(123456)
