0457af2a915bafccaeddfd60c621dd299a58e547
Manoel Jorge <manoel.jmb1990@gmail.com>
2026-01-29 16:22:28 -0300

    Inclui exemplo de TypeError e uso da função type()

--- day_02/type_erro_checking.py
+++ day_02/type_erro_checking.py
@@ -1,6 +1,12 @@
-len(123456) 
+len("123456")
 # TypeError: object of type 'int' has no len()
 # acima é um exemplo de TypeError, que ocorre quando uma operação ou função é 
 # aplicada a um objeto de um tipo inadequado.
 
-size = len(123456)
+# Demonstra o uso da função type() para identificar os tipos de dados:
+# inteiro (int), ponto flutuante (float), booleano (bool) e NoneType
+print(type(123456))
+print(type(1.5))
+print(type(True))
+print(type(False))
+print(type(None))
