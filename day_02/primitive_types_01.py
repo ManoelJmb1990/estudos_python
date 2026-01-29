13592139a384c355d3bc21d5e36d3ba974b1b81a
Manoel Jorge <manoel.jmb1990@gmail.com>
2026-01-29 12:04:06 -0300

    Utilização dos tipos primitivos

---
+++ day_02/primitive_types_01.py
@@ -0,0 +1,31 @@
+#String Subscripting (Indexing)
+
+print("Hello"[1])
+# Imprime a primeira letra da string "Hello", que é "H"
+
+print("Hello"[-4])
+# Imprime a segunda letra da string "Hello", que é "e"
+# Usando índice negativo para acessar a segunda letra a partir do final da string
+
+
+
+#String
+
+print("123" + "345") # Imprime a concatenação das duas strings, resultando em "123345"
+
+
+#interger = Número inteiro
+print(123 + 345) # Imprime a soma dos dois números inteiros, resultando em 468
+
+
+#large integer = Número inteiro grande
+print(123_456_789)
+# Imprime o número inteiro grande 123456789, onde os underscores são usados para melhorar a legibilidade
+
+#float = Número de ponto flutuante (número decimal)
+print(3.14159)
+
+#boolean = Valor booleano (Verdadeiro ou Falso)
+print(True)
+print(False)
+
